8a58b967c89a4ddaf3fc80cdbd66d05dc781fe28
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-01 15:10:59 -0300

    sii

--- database/config/config.js
+++ database/config/config.js
@@ -9,17 +9,17 @@ module.exports =
   /*   "port" : process.env.DB_PORT */
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "culmen" ,
+    "password": "Popi1234" ,
+    "database": "culmen_shops" ,
+    "host": "mysql-culmen.alwaysdata.net" ,
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect":  "mysql"
+    "username": "culmen" ,
+    "password": "Popi1234" ,
+    "database": "culmen_shops" ,
+    "host": "mysql-culmen.alwaysdata.net" ,
+    "dialect": "mysql"
   }
 };
