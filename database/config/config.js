5b07d5a49e871c82ff3312824e93813282583b82
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-01 14:55:43 -0300

    sa1111

--- database/config/config.js
+++ database/config/config.js
@@ -4,7 +4,7 @@ module.exports =
     "username": "culmen" || process.env.DB_USER,
     "password": "Popi1234" || process.env.PASSWORD,
     "database": "culmen_shops" || process.env.DB_NAME,
-    "host": mysql-culmen.alwaysdata.net ||  process.env.DB_HOST,
+    "host": "mysql-culmen.alwaysdata.net" ||  process.env.DB_HOST,
     "dialect": "mysql",
     "port" : process.env.DB_PORT
   },
