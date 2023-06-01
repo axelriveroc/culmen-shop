53a544a8db172c2acff7cd7880d90dc580742449
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-01 11:07:59 -0300

    dale

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -15,10 +15,6 @@ const Clothes_type = db.Clothes_type;
 const Brand_product = db.Brand_product;
 const Waist = db.Waist; 
 
-//const productsJSON = fs.readFileSync(path.join(__dirname , '../data/products.json') , 'utf-8');
-let products = JSON.parse(productsJSON); 
-
-
 const productsController = {
 
    index: async(req, res)=>{
@@ -76,12 +72,6 @@ const productsController = {
         }
     }, 
 
-
-   /*  detail: (req , res )=>{
-        const elegido = products.find( p => p.id == req.params.id );
-        res.render('products/productDetail' , { elegido } )
-    },  */
-
     detail: async(req, res)=>{
 
         try{
@@ -95,35 +85,6 @@ const productsController = {
         console.log(err)
        }
     },
-
-    store: (req, res)=>{
-
-        //const mainImage = req.files.file_img[0];
-        const nuevoId = products ? products[products.length - 1].id + 1 : 1 ;
-        //const image = req.files.file_img > 0 ? mainImage.filename : 'image.png';
-
-        const image = req.files.file_img[0].filename ? req.files.file_img[0].filename : 'image.png';
-
-        const secondaryImages = req.files.files_img;
-        const onlyFilenames = secondaryImages.map( img => {
-            const filename = img.filename || 'image.png';
-            return { filename };
-        })
-
-        console.log(onlyFilenames)
-
-        const newProduct = {
-            id: nuevoId , 
-            ...req.body , 
-            imagen: image,  
-            imagenes: onlyFilenames
-        }
-
-        products.push(newProduct);
-        //fs.writeFileSync( path.join(__dirname , '../data/products.json') , JSON.stringify(products, null, 2) )
-        res.redirect('/products')
-    }, 
-
     store2 : async ( req, res ) => {
 
         try{
@@ -240,29 +201,6 @@ const productsController = {
         }
     }, 
 
-    update: (req , res)=>{
-       
-        for (let i = 0; i < products.length; i++) {
-            if (products[i].id == req.params.id) {
-                if (req.file) {
-                   fs.unlinkSync(path.join(__dirname , `../public/products/${products[i].imagen}`))
-                } 
-                //const image = req.files.file_img > 0 ? req.files.file_img[0].filename : 'image.png';
-            const image = req.files.file_img[0].filename ? req.files.file_img[0].filename : products[i].imagen;
-            const secondaryImages = req.files.files_img;
-
-                products[i] = {
-                    id: req.params.id, 
-                    ...req.body, 
-                    imagen: image, 
-                    imagenes: secondaryImages ? secondaryImages : [{filename: 'image.png' }]
-                }; 
-            }
-        };
-        //fs.writeFileSync(  path.join(__dirname , '../data/products.json') , JSON.stringify(products, null, 2)); 
-        res.redirect('/products'); 
-    }, 
-
     update2: async(req, res)=>{
 
         try{
@@ -363,27 +301,6 @@ const productsController = {
         }
 
     },
-
-    delete: (req, res)=>{
-
-        const productsFiltrados = products.filter( p => p.id != req.params.id ); 
-        const elegido = products.find( p => p.id == req.params.id );
-        
-         if ( elegido.imagen != 'image.png' ) {
-             console.log(elegido.imagen)
-            fs.unlinkSync(path.join(__dirname , `../public/products/${elegido.imagen}`))
-        } 
-
-        if ( elegido.imagenes.length > 0 ) {
-            for (let i = 0; i < elegido.imagenes.length; i++) {
-                fs.unlinkSync(path.join(__dirname , `../public/products/${ elegido.imagenes[i].filename }`));
-            }
-        }
-
-        products = productsFiltrados; 
-        //fs.writeFileSync(  path.join(__dirname , '../data/products.json') , JSON.stringify(products , null, 2)); 
-        res.redirect('/products'); 
-    } , 
 
     delete2: async(req, res)=>{
         // PRIMERO TENGO QUE ELIMINAR LAS RELACIONES
