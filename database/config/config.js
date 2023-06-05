ba7dee7cde486129a18fea91cdbfd4511bdccb3a
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-05 11:25:11 -0300

    modificaciones en config

--- database/config/config.js
+++ database/config/config.js
@@ -1,12 +1,12 @@
 module.exports =
 {
   "development": {
-    "username": "culmen" ,
-    "password": "Popi1234" ,
-    "database": "culmen_shops" ,
-    "host": "mysql-culmen.alwaysdata.net" ,
-    "dialect": "mysql"
-  /*   "port" : process.env.DB_PORT */
+    "username": process.env.DB_USER ,
+    "password":process.env.DB_PASSWORD ,
+    "database": process.env.DB_NAME ,
+    "host": process.env.DB_HOST ,
+    "dialect": "mysql",
+    "port" : process.env.DB_PORT 
   },
   "test": {
     "username": process.env.DB_USER ,
