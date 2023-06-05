0efb89d86ac50e7bc3766062b53522052ef17e44
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-05 11:56:40 -0300

    tratando de hacer la automigracion con sequelize

--- database/models/index.js
+++ database/models/index.js
@@ -13,7 +13,17 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    logging: console.log, // Opcional: muestra los logs de las migraciones en la consola
+    define: {
+      timestamps: false // Opcional: deshabilita la creación de campos de timestamp en las tablas
+    },
+    sync: {
+      alter: true // Habilita la auto-migración
+    }
+  }
+  );
 }
 
 fs
