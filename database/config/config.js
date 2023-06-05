bb224e8b8dbef13879ae9d7ebd51f9b03d17ed1a
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-05 16:27:54 -0300

    config cambiado

--- database/config/config.js
+++ database/config/config.js
@@ -1,12 +1,12 @@
 module.exports =
 {
   "development": {
-    "username": process.env.DB_USER ,
-    "password":process.env.DB_PASSWORD ,
-    "database": process.env.DB_NAME ,
-    "host": process.env.DB_HOST ,
-    "dialect": "mysql",
-    "port" : process.env.DB_PORT 
+    "username": "root" ,
+    "password":null,
+    "database": "mydb" ,
+    "host": "127.0.0.1" ,
+    "dialect": "mysql"
+    
   },
   "test": {
     "username": process.env.DB_USER ,
