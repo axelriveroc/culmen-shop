e1cda01e548f820007d9bdf89e0962fdaa8a84c7
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-07 15:45:45 -0300

    nueva apikey

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,9 +8,9 @@ const cloudinary = require("cloudinary").v2;
 
 // Configuration
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_API_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name:process.env.CLOUDINARY_API_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET,
 });
 
 // SEQUELIZE 
