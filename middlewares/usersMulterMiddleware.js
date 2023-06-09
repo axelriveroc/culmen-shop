f291c02ff960956af7c07149cab7e8cb707a7f46
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-09 13:54:09 -0300

    agregando la variable url de cloudinary

--- middlewares/usersMulterMiddleware.js
+++ middlewares/usersMulterMiddleware.js
@@ -5,15 +5,22 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 
-const storage2 = new CloudinaryStorage({
+/* const storage2 = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "users",
     public_id: (req, file) => "computed-filename-using-request",
   },
+}); */
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "avatar",
+  },
 });
 
-const parser = multer({ storage: storage2 });
+const parser = multer({ storage: storage });
 
 module.exports =parser;
 
