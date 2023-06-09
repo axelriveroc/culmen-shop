ccd2c9fdee0302041657ff877e7247dd333714bb
axelriveroc <riverocoronelaxel@gmail.com>
2023-06-09 11:22:32 -0300

    asd

--- middlewares/usersMulterMiddleware.js
+++ middlewares/usersMulterMiddleware.js
@@ -1,6 +1,7 @@
 const path = require('path'); 
 const multer = require('multer');
-const { checkFileType } = require('./helpers');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 
 const storage2 = new CloudinaryStorage({
@@ -43,10 +44,15 @@ module.exports =parser;
         const newFilename = 'avatar-' + Date.now() + path.extname(file.originalname);
         cb( null , newFilename ); 
     }
-}); 
+
+});  */
+
 
 /* const upload = multer({ 
   storage
   , fileFilter  });  */
 
-module.exports = upload; 
+
+
+module.exports = parser; 
+
